18b4f4f446458099e69a1f34e0dae876c1163f13
Andrey Zakharov <mediaexplorer74@hotmail.com>
2024-12-10 11:57:45 +0300

    .

--- Src/Fooxboy.MusicX.AndroidApp/Delegates/EventHandler.cs
+++
@@ -1,5 +0,0 @@
-namespace Fooxboy.MusicX.AndroidApp.Delegates
-{
-    public delegate void EventHandler<T>(object sender, T args);
-    public delegate void EventHandler<T, T2>(object sender, T args, T2 block);
-}
--- Src/Fooxboy.MusicX.AndroidApp/Interfaces/IItemClickListener.cs
+++
@@ -1,9 +0,0 @@
-using Android.Views;
-
-namespace Fooxboy.MusicX.AndroidApp.Interfaces
-{
-    public interface IItemClickListener
-    {
-        void OnClick(View itemView, int position, bool isLongClick);
-    }
-}
